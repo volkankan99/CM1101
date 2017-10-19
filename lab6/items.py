b250814d110e056e3b0f50ed32353c740f1cf2d6
volkankan99 <volkankan99@gmail.com>
2017-10-20 00:56:28 +0100

    here

---
+++ lab6/items.py
@@ -0,0 +1,63 @@
+item_id = {
+    "id": "id",
+
+    "mass":0.5,
+
+    "name": "id card",
+
+    "description":
+    """You new shiny student ID card. Expires 1 June 2017.
+You wonder why they have printed a suicide hotline number on it?..."""
+}
+
+item_laptop = {
+    "id": "laptop",
+
+    "mass": 0.5,
+
+    "name": "laptop",
+
+    "description":
+    "It has seen better days. At least it has a WiFi card!"
+}
+
+item_money = {
+    "id": "money",
+
+    "mass": 0.5,
+
+    "name": "money",
+
+    "description":
+    "This wad of cash is barely enough to pay your tuition fees."
+}
+
+item_biscuits = {
+    "id": "biscuits",
+
+    "mass": 0.5,
+
+    "name": "a pack of biscuits",
+
+    "description": "A pack of biscuits."
+}
+
+item_pen = {
+    "id": "pen",
+
+    "mass": 0.5,
+    
+    "name": "a pen",
+
+    "description": "A basic ballpoint pen."
+}
+
+item_handbook = {
+    "id": "handbook",
+
+    "mass": 0.5,
+    
+    "name": "a student handbook",
+
+    "description": "This student handbook explains everything. Seriously."
+}
